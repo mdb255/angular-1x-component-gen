6213d5f8a499a3191c363941ea137fc6ada48c04
Mike Baldwin <mdb255.dev@gmail.com>
2018-02-11 22:27:53 -0500

    Store plugin settings in plugin dir; Fix template bug; Create subdir for each generated component

---
+++ hooks/post-install.js
@@ -0,0 +1,14 @@
+const os = require('os');
+const fs = require('fs');
+const path = require('path');
+
+const pluginDir = path.join(os.homedir(), ".ng-1x-cmp-gen");
+const configFile = path.join(pluginDir, "config.json");
+
+if (!fs.existsSync(pluginDir)) {
+  fs.mkdirSync(pluginDir, 755);
+}
+
+fs.copyFileSync("config.json", configFile);
+
+console.log(`Created default config file: ${configFile}`);
--- index.js
+++ index.js
@@ -5,20 +5,18 @@ const SETTINGS_FILENAME = "settings.json";
 const camelCase = require('lodash.camelcase');
 const upperFirst = require('lodash.upperfirst');
 const Handlebars = require('handlebars');
-const fs = require('file-system');
+const fs = require('fs');
+const os = require('os');
 const path = require('path');
 const program = require('commander');
 
 program
   .version('0.1.0')
   .option('-g, --generate [component name]', 'generate component files')
-  .option('-s, --set-config [path]', 'set the config path')
   .parse(process.argv);
 
 if (program.generate) {
   runGenerate(program.generate);
-} else if (program.setConfig) {
-  runSetConfig(program.setConfig);
 } else {
   program.help();
 }
@@ -26,15 +24,7 @@ if (program.generate) {
 function runGenerate(cmpName) {
   console.log("Generating component:", cmpName);
 
-  const libSettings = JSON.parse(fs.readFileSync(path.join(__dirname, SETTINGS_FILENAME), 'utf8'));
-
-  let configPath = libSettings.configPath;
-  if (!fs.existsSync(configPath)) {
-    console.log("WARN: Config path in settings.json does not exist");
-    configPath = path.join(__dirname, "./ng-1x-cmp-gen.config");
-  }
-  console.log(`Using config: ${configPath}`);
-
+  const configPath = path.join(os.homedir(), ".ng-1x-cmp-gen/config.json");
   const genConfig = JSON.parse(fs.readFileSync(configPath, 'utf8'));
 
   const cmpNameCamel = camelCase(cmpName);
@@ -48,13 +38,17 @@ function runGenerate(cmpName) {
   };
 
   const fileNames = {
-    js: `${cmpName}.js`,
-    scss: `${cmpName}.scss`,
-    html: `${cmpName}.component.html`,
+    js: `${cmpName}/${cmpName}.js`,
+    scss: `${cmpName}/${cmpName}.scss`,
+    html: `${cmpName}/${cmpName}.component.html`,
   };
 
   console.log('Config:', genConfig);
   console.log();
+
+  if (!fs.existsSync(cmpName)) {
+    fs.mkdirSync(cmpName, 755);
+  }
 
   buildJsOutput();
   buildScssOutput();
@@ -67,17 +61,17 @@ function runGenerate(cmpName) {
 
   function buildJsOutput() {
     const template = Handlebars.compile(fs.readFileSync(path.join(__dirname, '/templates/component.js.tmpl'), 'utf8'));
-    fs.writeFile(fileNames.js, template(context));
+    fs.writeFileSync(fileNames.js, template(context));
   }
 
   function buildScssOutput() {
     const template = Handlebars.compile(fs.readFileSync(path.join(__dirname, '/templates/component.scss.tmpl'), 'utf8'));
-    fs.writeFile(fileNames.scss, template(context));
+    fs.writeFileSync(fileNames.scss, template(context));
   }
 
   function buildHtmlOutput() {
     const template = Handlebars.compile(fs.readFileSync(path.join(__dirname, '/templates/component.html.tmpl'), 'utf8'));
-    fs.writeFile(fileNames.html, template(context));
+    fs.writeFileSync(fileNames.html, template(context));
   }
 }
 
